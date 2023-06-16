c03a47b3747bc00e0a16bac25dddfbd0e2578734
Gorkem Muslu <muslu.gorkem@gmail.com>
2023-06-16 13:59:46 +0300

    first commit

---
+++ src/main/java/tr/edu/metu/sm703/termproject/TermProjectApplication.java
@@ -0,0 +1,13 @@
+package tr.edu.metu.sm703.termproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TermProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TermProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tr/edu/metu/sm703/termproject/TermProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package tr.edu.metu.sm703.termproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TermProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
