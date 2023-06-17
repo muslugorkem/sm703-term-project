0128ff22c926a7836063562940f7d22068112e80
Gorkem Muslu <muslu.gorkem@gmail.com>
2023-06-17 13:08:33 +0300

    updated deploy_aws.yml 60

--- src/main/java/tr/edu/metu/sm703/termproject/controller/HelloWorldController.java
+++ src/main/java/tr/edu/metu/sm703/termproject/controller/HelloWorldController.java
@@ -12,7 +12,7 @@ public class HelloWorldController {
 
     @GetMapping("/hello")
     public String helloWorld() {
-        return "Hello World from: " + environment +".";
+        return "Hi Everyone from: " + environment +".";
     }
 }
 
