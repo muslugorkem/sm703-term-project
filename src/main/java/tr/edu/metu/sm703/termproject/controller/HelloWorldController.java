436ac482fd505ba0efcddc6da77abf4a53feecd6
Gorkem Muslu <muslu.gorkem@gmail.com>
2023-06-17 11:19:36 +0300

    updated deploy_aws.yml 38

--- src/main/java/tr/edu/metu/sm703/termproject/controller/HelloWorldController.java
+++ src/main/java/tr/edu/metu/sm703/termproject/controller/HelloWorldController.java
@@ -1,14 +1,18 @@
 package tr.edu.metu.sm703.termproject.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 
+    @Value("${WHICH_ENV}")
+    private String environment;
+
     @GetMapping("/hello")
     public String helloWorld() {
-        return "Hello, World!";
+        return "Hello, World from: " + environment;
     }
 }
 
--- src/test/java/tr/edu/metu/sm703/termproject/controller/HelloWorldControllerTest.java
+++ src/test/java/tr/edu/metu/sm703/termproject/controller/HelloWorldControllerTest.java
@@ -1,14 +1,14 @@
 package tr.edu.metu.sm703.termproject.controller;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -19,8 +19,7 @@ public class HelloWorldControllerTest {
 
     @Test
     public void helloWorldReturnsHelloWorld() throws Exception {
-        ResultActions resultActions = mockMvc.perform(get("/hello"))
-            .andExpect(status().isOk())
-            .andExpect(content().string("Hello, World!"));
+        ResultActions resultActions = mockMvc.perform(get("/hello")).andExpect(status().isOk());
+
     }
 }
